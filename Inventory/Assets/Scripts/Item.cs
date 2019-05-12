9a1e241eac6525fe3777d10adaf5e49f815ce722
aprildenise <admdizonn@gmail.com>
2019-05-12 16:17:35 -0400

    init and basic movement

---
+++ Inventory/Assets/Scripts/Item.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Item")]
+public class Item : ScriptableObject
+{
+
+    public string itemName;
+
+    public Sprite itemSprite;
+    public Sprite itemSpriteII;
+
+    [TextArea]
+    public string itemDescription;
+    public int itemSize;
+    public int itemQuantity;
+    public ItemType itemType;
+
+
+}
+
+public enum ItemType
+{
+    Neither = 0,
+    Consumable = 1,
+    Equipable = 2
+}
---
+++ Inventory/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    // References
+    private Rigidbody2D rigidBody;
+
+
+    // For movement
+    [SerializeField] private float moveSpeed;
+    private float dashSpeed;
+    private Vector2 moveVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Setups
+        rigidBody = GetComponent<Rigidbody2D>();
+        if (rigidBody == null)
+        {
+            Debug.LogWarning("PlayerController cannot find a RigidBody2D component on the object it is attached to.", rigidBody);
+        }
+
+        dashSpeed = moveSpeed * 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get movement input
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput.normalized * moveSpeed;
+    }
+
+
+    private void FixedUpdate()
+    {
+        // Move the player based on the movement input
+        rigidBody.MovePosition(rigidBody.position + moveVelocity * Time.fixedDeltaTime);
+    }
+}
