438bfb65d6f079d2b129097d81f3680f939e882d
aprildenise <admdizonn@gmail.com>
2019-06-16 15:34:28 -0400

    backing up any changes made

--- Inventory/Assets/Scripts/Player.cs
+++ Inventory/Assets/Scripts/Player.cs
@@ -8,6 +8,21 @@ public class Player : MonoBehaviour
     private string playerName;
     private int totalHP;
     private int currentHP;
+    private int holdableWeight;
 
+    private Item equippedItem;
+
+
+
+    public int GetHoldableWeight()
+    {
+        return holdableWeight;
+    }
+
+
+    public void SetHoldableWeight(int newWeight)
+    {
+        holdableWeight = newWeight;
+    }
     
 }
